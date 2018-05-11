5a840144626c4d6fd0b76d137e08d8f24ceef2b1
Mario Germán Reyes C <mager19@gmail.com>
2018-05-11 14:59:57 -0500

    Creado template para tipo de trabajo archive.

--- taxonomy-tipo.php
+++ taxonomy-tipo.php
@@ -6,49 +6,52 @@
  *
  * @package portafolio
  */
-
-get_header();
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				// the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_title( '<h1 class="page-title">' ,'</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+get_header();?>
 <?php
-get_sidebar();
+		 if ( have_posts() ) : ?>
+
+				<header class="trabajos-header">
+					<div class="container">
+						<div class="row">
+							<div class="col-12">
+								<h2><img src="<?php echo get_template_directory_uri() ?>/img/article.png">Trabajos Realizados</h2>
+								<div class="archive-description">Aquí encontrarás los trabajos realizados según la categoría seleccionada: <strong><?php the_terms($post->ID, 'tipo', '',' - '); ?></strong></div>	
+							</div>
+						</div>
+					</div>					
+				</header><!-- .page-header -->
+				<div class="container">
+					<div class="row row-eq-height">
+						
+						<?php
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
+
+							/*
+							 * Include the Post-Type-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', 'tipoContenido-articulo' );
+
+						endwhile;
+
+						?>
+						<div class="col-12"><?php the_posts_navigation();?></div>
+					<?php
+					else :
+
+						get_template_part( 'template-parts/content', 'none' );
+
+					endif;
+					?>
+
+					</div><!-- #main -->
+					
+					<?php get_template_part( 'template-parts/content', 'trabajemos' ); ?>
+				</div><!-- #primary -->
+	
+<?php
+
 get_footer();
